809516dfde4ff12e69e05f396e6fd6970540d1b3
salrashed <isalrashed.29@gmail.com>
2024-11-18 11:54:36 +0300

    delete

--- controllers/items.js
+++ controllers/items.js
@@ -19,7 +19,20 @@ router.put('/', async (req, res) => {
 
 // DELETE - Remove
 router.delete('/', async (req, res) => {
-
+    try {
+        const deletedItem = await Item.findByIdAndDelete({ _id: req.params.itemId })
+        if (!deletedItem) {
+            res.status(404)
+            throw new Error('Item not found.')
+        }
+        res.status(200).json(deletedItem)
+    } catch (error) {
+        if (res.statusCode === 404) {
+            res.json({ error: error.message })
+        } else {
+            res.status(500).json({ error: error.message })
+        }
+    }
 })
 
 module.exports = router
