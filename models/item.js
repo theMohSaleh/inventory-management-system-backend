32375843ba6b899e7b445d65b538e0519ca4f3a1
Mohamed Saleh <mohxbox36021@gmail.com>
2024-11-18 10:57:14 +0300

    created item model

---
+++ models/item.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const itemSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: String,
+    quantity: {
+        type: Number,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    }
+})
+
+const Item = mongoose.model('Item', itemSchema);
+
+module.exports = Item;
