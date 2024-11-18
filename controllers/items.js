20b621d3e11f3712c32b67b65a6036b3cd0a9454
halbuflasa <albuflasahessa@gmail.com>
2024-11-18 13:33:08 +0300

    show item is completed

--- controllers/items.js
+++ controllers/items.js
@@ -2,6 +2,24 @@ const express = require('express');
 const verifyToken = require('../middleware/verify-token.js');
 const Item = require('../models/item.js')
 const router = express.Router();
+// ========= Public Routes ===========
+
+
+
+// ========= Protected Routes =========
+router.use(verifyToken);
+// POST - Create
+router.post('/', async (req, res) => {
+    try {
+        req.body.owner = req.user._id;
+        const item = await Item.create(req.body);
+        item._doc.owner = req.user;
+        res.status(201).json(item);
+    }catch (error){
+        console.error(error);
+        res.status(500).json({ error: 'An error occurred while creating the item' });
+    }
+});
 
 // GET - Index
 router.get('/', async (req, res) => {
@@ -19,22 +37,32 @@ router.get('/', async (req, res) => {
             res.status(500).json({ error: error.message });
         }
     }
-})
+});
 
-// ========= Protected Routes =========
-router.use(verifyToken);
-// POST - Create
-router.post('/', async (req, res) => {
+// GET - Show one item by itemId
+router.get('/:itemId', async (req, res) => {  
     try {
-        req.body.owner = req.user._id;
-        const item = await Item.create(req.body);
-        item._doc.owner = req.user;
-        res.status(201).json(item);
-    }catch (error){
-        console.error(error);
-        res.status(500).json({ error: 'An error occurred while creating the item' });
+        const foundItem = await Item.findById(req.params.itemId);
+        if (!foundItem) {
+            res.status(404);
+            throw new Error('Item not found.');
+          }
+        res.status(200).json(foundItem);
+    }catch(error){
+        if (res.statusCode === 404) {
+            res.json({ error: error.message });
+        } else {
+            res.status(500).json({ error: error.message });
+          }
     }
-})
+
+});
+
+
+
+
+
+
 
 // PUT - Update
 router.put('/', async (req, res) => {
