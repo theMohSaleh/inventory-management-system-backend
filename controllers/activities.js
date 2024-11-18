1c39270c6517b31d6b7759df03c1b173332605fa
Mohamed Saleh <mohxbox36021@gmail.com>
2024-11-18 14:47:47 +0300

    added items activity route

--- controllers/activities.js
+++ controllers/activities.js
@@ -20,4 +20,22 @@ router.get('/', async (req, res) => {
     }
 })
 
+// GET - get history of item
+router.get('/:itemId', async (req, res) => {
+    try {
+        const logsFound = await Activity.find({item: req.params.itemId});
+        if (!logsFound.length) {
+            res.status(404);
+            throw new Error('No logs found');
+        }
+        res.status(200).json(logsFound);
+    } catch (error) {
+        if (res.statusCode === 404) {
+            res.json({ error: error.message })
+        } else {
+            res.status(500).json({ error: error.message });
+        }
+    }
+})
+
 module.exports = router
