e939268cdad2cca8a7ef9f2f7055f484f1744e7b
Mohamed Saleh <mohxbox36021@gmail.com>
2024-11-18 13:19:21 +0300

    added activities controller and middleware for protected routes

---
+++ controllers/activities.js
@@ -0,0 +1,23 @@
+const Activity = require('../models/activity.js')
+const express = require('express')
+router = express.Router();
+
+// GET - Index
+router.get('/', async (req, res) => {
+    try {
+        const logsFound = await Activity.find();
+        if (!logsFound.length) {
+            res.status(404);
+            throw new Error('No logs found');
+        }
+        res.status(200).json(logsFound);
+    } catch (error) {
+        if (res.statusCode === 404) {
+            res.json({ error: error.message })
+        } else {
+            res.status(500).json({ error: error.message });
+        }
+    }
+})
+
+module.exports = router
